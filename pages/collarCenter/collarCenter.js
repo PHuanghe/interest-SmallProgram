09024389449759ac249301cf55490d63a5c48c0e
jiale_wang <935598437@qq.com>
2018-04-04 12:10:03 +0800

    分享页面封面图替换

---
+++ pages/collarCenter/collarCenter.js
@@ -0,0 +1,74 @@
+// pages/collarCenter/collarCenter.js
+var app = getApp(); 
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    var timer = setInterval(function () {
+      if (app.globalData.address != "中国" && app.globalData.tokenId) {
+        that.setData({
+          token: app.globalData.tokenId
+        })
+      }
+    }, 300)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
